5dac97dee440222223a0e954eaa9491ead52fce2
Emilio Ochoa <emilioaor@gmail.com>
2023-05-02 09:39:30 -0400

    Fix user pagination

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,5 +4,7 @@
     <user-list
         :items = "{{ json_encode($users->items()) }}"
         :total = "{{ $users->total() }}"
-    ></user-list>
+    >
+        <template v-slot:pagination>{{ $users->links() }}</template>
+    </user-list>
 @endsection
